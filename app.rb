cb4118eb8a54a046d9772d111caaef23a25ff76a
Luba Kassurova <kassurova@icloud.com>
2023-03-16 18:09:16 -0400

    commit tree 347deaddeb03d3a1c5ef140108c92952495c6e7d

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 require './environment'
 
+require "pry"
 module FormsLab
   class App < Sinatra::Base
+      get '/' do 
+      erb :root
+      end  
+  
+      get '/new' do
+      erb :"pirates/new"
+      end
 
-    # code other routes/actions here
+      
 
+    post '/pirates' do
+   
+       pirate_name = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+       @pirate_one = pirate_name
+      
+      
+       params[:pirate][:ship].each do |ship|
+      Ship.new(ship[:name],ship[:type], ship[:booty])
+
+      end
+    
+      @ships = Ship.all
+      erb :"pirates/show"
+     
+    end
   end
 end
